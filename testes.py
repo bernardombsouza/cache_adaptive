f821fe67d363dd7c12efe5fa1fc3e4fa2c07d9b7
Bernardo Magalhães <bernardombsouza@gmail.com>
2025-08-27 14:15:55 -0300

    initial testes functions cache

---
+++ testes.py
@@ -0,0 +1,34 @@
+import unittest
+from datetime import datetime, timedelta
+from freezegun import freeze_time
+import time
+
+# Substitua 'adaptive_cache' pelo nome do seu arquivo.
+from new_adaptive_cache import AdaptiveCache, CachePolicy 
+
+# Dados de teste para reutilização
+TEST_KEY = "test_key"
+TEST_VALUE = "test_value"
+LARGE_VALUE = "a" * 1024 # Valor grande para teste de memória (1MB)
+
+class TestAdaptiveCache(unittest.TestCase):
+    
+    def setUp(self):
+        """Prepara o ambiente para cada teste."""
+        # Cria uma nova instância do cache para cada teste
+        self.cache = AdaptiveCache(max_memory_mb=1000, compression_threshold_kb=1)
+
+# -----------------
+# Testes do método put()
+# -----------------
+    def test_put_adds_new_item(self):
+        """Testa se o método put adiciona um novo item ao cache."""
+        print('''TESTE DE INSERÇÃO DE VALOR...''')
+        self.cache.put(key=TEST_KEY, value=TEST_VALUE)
+        print("VALOR DENTRO DO CACHE TEM QUE SER test_value: ", self.cache.get(TEST_KEY), '\n')
+        self.assertIn(TEST_KEY, self.cache.cache_data)
+        self.assertEqual(self.cache.get(TEST_KEY), TEST_VALUE)
+
+
+if __name__ == '__main__':
+    unittest.main()
