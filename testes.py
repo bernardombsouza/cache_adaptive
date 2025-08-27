bfdd1fe9904e463dc8dc0b13ced9909316d863aa
Bernardo Magalhães <bernardombsouza@gmail.com>
2025-08-27 14:54:48 -0300

    add function memory limit

--- testes.py
+++ testes.py
@@ -37,7 +37,22 @@ class TestAdaptiveCache(unittest.TestCase):
         self.cache.put(TEST_KEY, "valor_novo")
         print("VALOR NOVO ", self.cache.get(TEST_KEY), '\n')
         self.assertEqual(self.cache.get(TEST_KEY), "valor_novo")
-
+    
+    def test_put_enforces_memory_limit(self):
+        """Testa se o cache remove itens quando atinge o limite de memória."""
+        print("TESTE DE LIMITE DE MEMÓRIA...")
+        for i in range(0, 150):
+            self.cache.put(f"key_{i}", TEST_VALUE)
+            key = f"key_{i}"
+        self.assertIn(key, self.cache.cache_data)
+        print("VALOR DA ULTIMA KEY QUE TENTEI ADICIONAR", self.cache.get(key))
+        # Adiciona outro item grande, o primeiro deve ser removido
+        self.cache.put("new_key", TEST_VALUE)
+        print("VALOR DA KEY GRANDE QUE ADICIONEI AGORA", self.cache.get(key))
+        self.assertNotIn("key_0", self.cache.cache_data)
+        print("CONFIRMA QUE A KEY NAO EXISTE: ", self.cache.get("key_0"))
+        self.assertIn("new_key", self.cache.cache_data)
+        print("CONFIRMA QUE A KEY NOVA FOI ADICIONADA: ", self.cache.get("new_key"), '\n')
 
 if __name__ == '__main__':
     unittest.main()
